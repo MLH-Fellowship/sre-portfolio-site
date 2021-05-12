5c05248f9013747663e9f5de8ac629c7a975bd66
William Russell <will.russell@mlh.io>
2021-05-12 16:59:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flask Portfolio Template
---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, render_template, send_from_directory
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
