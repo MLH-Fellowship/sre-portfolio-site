57d2db0b9d83712fcf6b4e4ebbb2f727b9e749f4
korusuke <karan.sheth@somaiya.edu>
2021-05-23 17:30:55 +0530

    Update requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
