b95d0b69412da182707f00fdf5cd4309e42b0727
jjayeon <autumn.chiu.136@gmail.com>
2021-06-09 11:11:32 -0400

    added basic routing; changed asset links to be absolute paths

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellow",
+                           bodytext="it me", url=os.getenv("URL"))
+
+@app.route('/experience/')
+@app.route('/experience/<page>')
+def experience(page=None):
+    return render_template('index.html', title="Experience",
+                           bodytext="experience?", url=os.getenv("URL"))
+
+@app.route('/projects/')
+@app.route('/projects/<page>')
+def projects(page=None):
+    return render_template('index.html', title="Projects",
+                           bodytext="projects?", url=os.getenv("URL"))
+
+@app.route('/accomplishments/')
+@app.route('/accomplishments/<page>')
+def accomplishments(page=None):
+    return render_template('index.html', title="Accomplishments",
+                           bodytext="accomplishments?", url=os.getenv("URL"))
+
+@app.route('/skills/')
+@app.route('/skills/<page>')
+def skills(page=None):
+    return render_template('index.html', title="Skills",
+                           bodytext="skills?", url=os.getenv("URL"))
+
+        
