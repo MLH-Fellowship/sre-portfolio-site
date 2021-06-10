4ee1f1f70fa9761069039d3a0e1551f86c2c738e
jjayeon <autumn.chiu.136@gmail.com>
2021-06-10 14:08:18 -0400

    decompose html/css doc into templates

--- app/__init__.py
+++ app/__init__.py
@@ -12,20 +12,24 @@ f.close()
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow",
-                           content="it me", url=os.getenv("URL"))
+    return render_template('data.html',
+                           data=data,
+                           main="Projects",
+                           side1="Experience",
+                           side2="Accomplishments",
+                           url=os.getenv("URL"))
 
-@app.route('/experience/')
-def experience():
-    return render_template('data.html', title="Experience",
-                           content=data["experience"], url=os.getenv("URL"))
+# @app.route('/experience/')
+# def experience():
+#     return render_template('data.html', title="Experience",
+#                            content=data["experience"], url=os.getenv("URL"))
 
-@app.route('/projects/')
-def projects():
-    return render_template('index.html', title="Projects",
-                           content="projects?", url=os.getenv("URL"))
+# @app.route('/projects/')
+# def projects():
+#     return render_template('index.html', title="Projects",
+#                            content="projects?", url=os.getenv("URL"))
 
-@app.route('/accomplishments/')
-def accomplishments():
-    return render_template('index.html', title="Accomplishments",
-                           content="accomplishments?", url=os.getenv("URL"))
+# @app.route('/accomplishments/')
+# def accomplishments():
+#     return render_template('index.html', title="Accomplishments",
+#                            content="accomplishments?", url=os.getenv("URL"))
