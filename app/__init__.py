54768797b36d2feb0524a471e8359cc9df8af70a
Cloud User <centos@ip-172-31-44-156.ap-south-1.compute.internal>
2021-06-19 13:30:46 +0000

    Add gunicorn support

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template, send_from_directory
-from dotenv import load_dotenv
 
-load_dotenv()
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellow", url="localhost:5000")
+
+@app.route('/health')
+def health():
+   return "Works"
