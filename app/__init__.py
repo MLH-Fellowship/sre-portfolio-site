52afe3610a04e1558f146d359100307765827f79
Leeia Isabelle <isabelle.leeia@gmail.com>
2021-07-03 14:22:28 -0700

    Added database configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 
 @app.route('/')
 def index():
