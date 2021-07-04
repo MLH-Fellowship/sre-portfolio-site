d5cbe70fbabf42f2588930fdc604dbcd42837bad
Leeia Isabelle <isabelle.leeia@gmail.com>
2021-07-03 19:51:05 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask, request, render_template, send_from_directory
 from dotenv import load_dotenv
 from . import db
 
-load_dotenv()
 app = Flask(__name__)
 app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 db.init_app(app)
