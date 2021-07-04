217cd15d0a7a06064fdf572c5f4d14ccc379cb04
Cloud User <centos@ip-172-31-88-14.ec2.internal>
2021-07-04 01:59:27 +0000

    update test_endpoints.sh to use new URLs

--- test_endpoints.sh
+++ test_endpoints.sh
@@ -8,8 +8,8 @@ url=$1
 printf "\n ~~ creating flask db ~~ \n"
 flask init-db
 printf "\n ~~ curling login POST (should be error, no users yet) ~~ \n"
-curl -X POST -d "username=JonSmith&password=pw123" "${url}/login"
+curl -X POST -d "username=JonSmith&password=pw123" "${url}/mlh/login"
 printf "\n ~~ curling register POST (should be successful) ~~ \n"
-curl -X POST -d "username=JonSmith&password=pw123" "${url}/register"
+curl -X POST -d "username=JonSmith&password=pw123" "${url}/mlh/register"
 printf "\n ~~ curling login POST (should now be successful) ~~ \n"
-curl -X POST -d "username=JonSmith&password=pw123" "${url}/login"
+curl -X POST -d "username=JonSmith&password=pw123" "${url}/mlh/login"
