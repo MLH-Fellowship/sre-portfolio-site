b328501470d1c94adedd28525cb1008a8230775d
jjayeon <autumn.chiu.136@gmail.com>
2021-07-08 07:10:56 -0400

    reduce gunicorn workers from 4 to 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 flask db migrate
 flask db upgrade
-gunicorn wsgi:app -w 4 -b 0.0.0.0:80 --capture-output --log-level debug
+gunicorn wsgi:app -w 1 -b 0.0.0.0:80 --capture-output --log-level debug
