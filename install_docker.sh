42399a5d923f616af66c4326657b2415cbae7ed7
jjayeon <autumn.chiu.136@gmail.com>
2021-07-16 10:38:12 -0400

    fix some mistakes with git commits

--- install_docker.sh
+++ install_docker.sh
@@ -10,7 +10,7 @@ if ! command -v docker &> /dev/null
 then
     curl -sSL https://get.docker.com/ | sh
 fi
-if ! command -v docker-compose & /dev/null
+if ! command -v docker-compose &> /dev/null
 then
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
